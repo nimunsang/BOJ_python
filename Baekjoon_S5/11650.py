0e672003bfcb4b5340007013395cda33b19d4ff0
Kong HeeChan <sk980919@naver.com>
2021-10-20 16:32:39 +0900

    first commit

--- Baekjoon_S5/11650.py
+++ Baekjoon_S5/11650.py
@@ -2,11 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = []
 
+lst = []
 for i in range(N):
     x, y = map(int, input().split())
-    lst.append(x, y)
+    lst.append((x, y))
 
-lst.sort(key= lambda x:(x[0], x[1]))
-print(lst)
+lst.sort()
+
+for x, y in lst:
+    print(x, y)
