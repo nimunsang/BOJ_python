3627b4bbe6a8a8e9e5c21c73b719a59ed3827fb4
Kong Heechan <64733547+nimunsang@users.noreply.github.com>
2021-11-18 17:03:35 +0900

    211118

---
+++ Baekjoon_S1/1697.py
@@ -0,0 +1,26 @@
+#다시 #그래프이론 #그래프탐색 #BFS
+"""
+https://www.acmicpc.net/problem/1697
+[1697] : 숨바꼭질
+"""
+
+from collections import deque
+
+N, K = map(int, input().split())
+visited = [0] * 100001
+
+def bfs():
+    deq = deque()
+    deq.append(N)
+    while deq:
+        d = deq.popleft()
+        if d == K:
+            print(visited[K])
+            break
+
+        for dn in (d-1, d+1, d*2):
+            if 0<=dn<=100000 and visited[dn] == 0:
+                visited[dn] = visited[d] + 1
+                deq.append(dn)
+
+bfs()
