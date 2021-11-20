d577fb8acd4e8b6ac8f187852186a6ae9f71885d
Kong HeeChan <sk980919@naver.com>
2021-11-20 18:18:39 +0900

    211120

---
+++ Baekjoon_S1/10844.py
@@ -0,0 +1,25 @@
+#다시 #다이나믹프로그래밍
+"""
+https://www.acmicpc.net/problem/10844
+[10844] : 쉬운 계단 수
+"""
+
+N = int(input())
+
+dp = [[0] * (10) for _ in range(N+1)]
+
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for i in range(2, N+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[N]) % (10**9))
