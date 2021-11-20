117cac39250ce195bc856f051d2108f05a2d7bd4
Kong HeeChan <sk980919@naver.com>
2021-11-20 19:48:59 +0900

    211120

--- RetryFiles/1463.py
+++ RetryFiles/1463.py
@@ -4,15 +4,17 @@ https://www.acmicpc.net/problem/1463
 [1463] : 1로 만들기
 """
 
+import sys
+input = sys.stdin.readline
 N = int(input())
 
 dp = [0] * (N+1)
 
+
 for i in range(2, N+1):
     dp[i] = dp[i-1] + 1
     if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-    if i%2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-
+        dp[i] = min(dp[i//3]+1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2]+1, dp[i])
 print(dp[N])
