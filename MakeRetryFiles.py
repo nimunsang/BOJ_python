53893fae8bdb306b7116fa7228d29a809ce68ef9
Kong HeeChan <sk980919@naver.com>
2021-11-21 21:44:45 +0900

    211121

--- MakeRetryFiles.py
+++ MakeRetryFiles.py
@@ -1,4 +1,6 @@
 #다시 풀어야하는 문제들을 불러오는 코드
+#다시 풀어야하는 문제로 새로운 파일을 생성하고,
+#그 문제의 링크를 불러온다.
 
 import os
 import shutil
@@ -11,13 +13,24 @@ for i in range(len(files)):
     if 'Baekjoon' in files[i]:
         baekjoonfiles.append(files[i])
 
+
 for baekjoon in baekjoonfiles:
+    #(ex) path = 'C:/Users/samsung/Desktop/myPython/Baekjoon_S1'   
     path = origin + '/' + baekjoon
     files = os.listdir(path)
     for file in files:
         filepath = path + '/' + file
         f = open(filepath, 'r', encoding = 'utf8')
         l = f.readline()
+        #첫번째 줄을 l로 불러온다.
+        # '#다시'가 첫번째 줄에 있다면, 모든 줄을 불러온다
         if '#다시' in l:
-            shutil.copy(filepath, copy + '/' + file)
+            lines = f.readlines()
+            for line in lines:
+                if 'https' in line:
+                    with open(copy + '/' + file, 'w') as newfile:
+                        newfile.write(line)
+                        newfile.close()
+                        break
+                        
         f.close()
