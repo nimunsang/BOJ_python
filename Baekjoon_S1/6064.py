911007bc53018bd031cceb08dd54e95e55ee4d00
Kong HeeChan <sk980919@naver.com>
2021-12-04 15:26:24 +0900

    211204

---
+++ Baekjoon_S1/6064.py
@@ -0,0 +1,22 @@
+#다시 #수학 #정수론 #중국인의나머지정리
+"""
+https://www.acmicpc.net/problem/6064
+[6064] : 카잉 달력
+"""
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for i in range(T):
+    M, N, x, y = map(int, input().split())
+
+    while x <= N*M:
+        if (x-y) % N == 0:
+            print(x)
+            break
+
+        x += M
+    
+    else:
+        print(-1)
