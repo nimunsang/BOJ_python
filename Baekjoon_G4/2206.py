54af105caf593df91f038e9e3290e4cf5bff09b7
Kong Heechan <sk980919@naver.com>
2022-01-14 20:08:07 +0900

    220114

---
+++ Baekjoon_G4/2206.py
@@ -0,0 +1,41 @@
+#다시 #그래프이론 #그래프탐색 #BFS
+"""
+https://www.acmicpc.net/problem/2206
+[2206] : 벽부수고 이동하기
+"""
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().rstrip())) for _ in range(N)]
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def bfs():
+    q = deque()
+    q.append([0, 0, 1])
+    visited = [[[0]*2 for _ in range(M)] for __ in range(N)]
+    visited[0][0][1] = 1
+    while q:
+        a, b, w = q.popleft()
+        if a == N-1 and b == M-1:
+            return visited[a][b][w]
+
+        for i in range(4):
+            x = b + dx[i]
+            y = a + dy[i]
+            if 0<=x<M and 0<=y<N:
+                if arr[y][x] == 1 and w == 1:
+                    visited[y][x][0] = visited[a][b][w] + 1
+                    q.append([y, x, 0])
+                elif arr[y][x] == 0 and visited[y][x][w] == 0:
+                    q.append([y, x, w])
+                    visited[y][x][w] = visited[a][b][w]+1
+                
+    return -1
+
+a = bfs()
+print(a)
---
+++ Baekjoon_G4/2580.py
@@ -0,0 +1,51 @@
+#다시 #백트래킹
+"""
+https://www.acmicpc.net/problem/2580
+[2580] : 스도쿠
+"""
+
+arr = [list(map(int, input().split())) for _ in range(9)]
+blank = []
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == 0:
+            blank.append((i, j))
+
+def checkrow(y, a):
+    for i in range(9):
+        if arr[y][i] == a:
+            return False
+    return True
+
+def checkcol(x, a):
+    for i in range(9):
+        if arr[i][x] == a:
+            return False
+    return True
+    
+def checksq(y, x, a):
+    y //= 3
+    x //= 3
+    for i in range(y*3, y*3+3):
+        for j in range(x*3, x*3+3):
+            if arr[i][j] == a:
+                return False
+    
+    return True
+
+def dfs(idx):
+    if len(blank) == idx:
+        for i in range(9):
+            print(*arr[i])
+        exit(0)
+    
+    for i in range(1, 10):
+        x = blank[idx][1]
+        y = blank[idx][0]
+
+        if checkrow(y, i) and checkcol(x, i) and checksq(y, x, i):
+            arr[y][x] = i
+            dfs(idx+1)
+            arr[y][x] = 0
+
+dfs(0)
