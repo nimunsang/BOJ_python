456f1dcce8ab36d3d4cc9815de114472e476e9e6
Kong Heechan <sk980919@naver.com>
2022-01-19 20:13:22 +0900

    220119

--- Baekjoon_G4/11404.py
+++ Baekjoon_G4/11404.py
@@ -10,7 +10,7 @@ input = sys.stdin.readline
 n = int(input())
 m = int(input())
 inf = 10**9
-graph = [[inf] * (n+1) for _ in range(n+1)]
+graph = [[inf]*(n+1) for _ in range(n+1)]
 for i in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = min(graph[a][b], c)
@@ -19,8 +19,12 @@ for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-            if i == j:
-                graph[i][j] = 0
 
 for i in range(1, n+1):
-    print(*graph[i][1:])
+    for j in range(1, n+1):
+        if i == j:
+            print(0, end = " ")
+
+        else:
+            print(graph[i][j] if graph[i][j] != inf else 0, end = " ")
+    print("")
